c479742b659bdd729740b4bc0ce2c5a2ea655459
Polina Komarova <polina-komarova-95@mail.ru>
2023-09-30 00:41:29 +0300

    .env.deploy.example added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,6 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-// Только для локальных тестов. Не используйте это в продакшене
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
